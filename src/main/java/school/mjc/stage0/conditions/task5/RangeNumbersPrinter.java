bf39fe24f4f73f0694898f60f030ebd7898641c9
nikitakorneichuk <korneichuk.nikita@gmail.com>
2023-06-30 17:08:21 +0300

    Add some code

--- src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/RangeNumbersPrinter.java
@@ -2,5 +2,43 @@ package school.mjc.stage0.conditions.task5;
 
 public class RangeNumbersPrinter {
     public void printRange(int number) {
+        String answer;
+        switch (number) {
+            case (1):
+                answer = "number is between 1 and 5";
+                break;
+            case (2):
+                answer = "number is between 1 and 5";
+                break;
+            case (3):
+                answer = "number is between 1 and 5";
+                break;
+            case (4):
+                answer = "number is between 1 and 5";
+                break;
+            case (5):
+                answer = "number is between 1 and 5";
+                break;
+            case (6):
+                answer = "number is between 6 and 10";
+                break;
+            case (7):
+                answer = "number is between 6 and 10";
+                break;
+            case (8):
+                answer = "number is between 6 and 10";
+                break;
+            case (9):
+                answer = "number is between 6 and 10";
+                break;
+            case (10):
+                answer = "number is between 6 and 10";
+                break;
+            default:
+                answer = "out of range";
+                break;
+
+        }
+        System.out.println(answer);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,48 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        String days;
+        switch (month) {
+            case (1):
+                days = "31";
+                break;
+            case(2):
+                days = "28";
+                break;
+            case(3):
+                days = "31";
+                break;
+            case(4):
+                days = "30";
+                break;
+            case(5):
+                days = "31";
+                break;
+            case(6):
+                days = "30";
+                break;
+            case(7):
+                days = "31";
+                break;
+            case(8):
+                days = "31";
+                break;
+            case(9):
+                days = "30";
+                break;
+            case(10):
+                days = "31";
+                break;
+            case(11):
+                days = "30";
+                break;
+            case(12):
+                days = "31";
+                break;
+            default:
+                days = "wrong number!";
+                break;
+        }
+        System.out.println(days);
     }
 }
